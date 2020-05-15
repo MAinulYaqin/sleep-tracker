defedf3ce3305e6b669116d3c2a08e45c4a2dce1
MAinulYaqin <ainul.y9b@gmail.com>
2020-05-15 21:15:33 +0700

    adding functionality to SleepTrackerViewModel and sleep_tracker_fragment

--- app/src/main/java/com/gabutproject/napapp/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/gabutproject/napapp/sleeptracker/SleepTrackerFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.gabutproject.napapp.R
+import com.gabutproject.napapp.database.SleepDatabase
 import com.gabutproject.napapp.databinding.FragmentSleepTrackerBinding
 
 class SleepTrackerFragment : Fragment() {
@@ -21,6 +23,19 @@ class SleepTrackerFragment : Fragment() {
             container,
             false
         )
+
+        val application = requireNotNull(this.activity).application
+
+        val dataSource = SleepDatabase.getInstance(application).sleepDatabaseDao
+
+        val viewModelFactory = SleepTrackerViewModelFactory(dataSource, application)
+
+        val viewModel: SleepTrackerViewModel =
+            ViewModelProvider(this, viewModelFactory).get(SleepTrackerViewModel::class.java)
+
+        binding.viewModel = viewModel
+
+        binding.lifecycleOwner = this
 
         return binding.root
     }
---
+++ app/src/main/java/com/gabutproject/napapp/sleeptracker/SleepTrackerViewModel.kt
@@ -0,0 +1,105 @@
+package com.gabutproject.napapp.sleeptracker
+
+import android.app.Application
+import android.os.SystemClock
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import com.gabutproject.napapp.database.SleepDatabaseDao
+import com.gabutproject.napapp.database.SleepNight
+import com.gabutproject.napapp.formatNights
+import kotlinx.coroutines.*
+
+class SleepTrackerViewModel(private val database: SleepDatabaseDao, application: Application) :
+    AndroidViewModel(application) {
+
+    private var viewModelJob = Job()
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+
+    // viewModel update the UI with LiveData after some processing
+    // so we need the Coroutine to run in the Main thread
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    private var _tonight = MutableLiveData<SleepNight?>()
+
+    private val nights = database.getAllNights()
+
+    var nightsString = Transformations.map(nights) {
+        formatNights(it, application.resources)
+    }
+
+    init {
+        initializeNights()
+    }
+
+    private fun initializeNights() {
+        uiScope.launch {
+            _tonight.value = getTonightFromDatabase()
+        }
+    }
+
+    private suspend fun getTonightFromDatabase(): SleepNight? {
+        return withContext(Dispatchers.IO) {
+            var night = database.getTonight()
+
+            // initially, the endTime & startTime is the same value
+            // but the endTime will updated when button stop presses
+            // so if the user start the app and close it, then restart it in the morning, there's no value
+            if (night?.endTimeMillis != night?.startTimeMillis) {
+                night = null
+            }
+
+            night
+        }
+    }
+
+    fun onStartTicking() {
+        uiScope.launch {
+            var newNight = SleepNight()
+
+            insert(newNight)
+
+            _tonight.value = getTonightFromDatabase()
+        }
+    }
+
+    private suspend fun insert(night: SleepNight) {
+        withContext(Dispatchers.IO) {
+            database.insert(night)
+        }
+    }
+
+    fun onStopTicking() {
+        uiScope.launch {
+            // null check, and set endTime to current Time
+            val oldNight = _tonight.value ?: return@launch
+            oldNight.endTimeMillis = System.currentTimeMillis()
+
+            update(oldNight) // update database
+        }
+    }
+
+    private suspend fun update(night: SleepNight) {
+        withContext(Dispatchers.IO) {
+            database.update(night)
+        }
+    }
+
+    fun onClear() {
+        uiScope.launch {
+            clear()
+            _tonight.value = null
+        }
+    }
+
+    private suspend fun clear() {
+        withContext(Dispatchers.IO) {
+            database.clear()
+        }
+    }
+}
